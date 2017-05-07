22beabb36a630c0f0cfc51124c72af23f714f9b7
Techbird <techbirdsolutions@outlook.com>
2017-05-07 13:33:24 +0530

    master file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#bootstrap-touch-slider').bsTouchSlider();
+
+});
+
+$(document).on("click",".sidebar-toggle",function(){
+    $(".wrapper").toggleClass("toggled");
+
+});
